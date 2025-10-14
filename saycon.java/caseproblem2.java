3fce32b30f425b34380a7156bad6e18b6fe8adf4
sayconjennifer02-ui <sayconjennifer02@gmail.com>
2025-10-14 17:21:21 +0800

    Uploaded Java case problem and output screenshot

---
+++ saycon.java/caseproblem2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class caseproblem2 {
+    public static void main(String[] args) {
+        String name = "Jennifer M. Saycon";
+        String idNumber = "2021309600";
+        int num1 = 10;
+        int num2 = 20;
+        int sum = num1 + num2;
+
+        System.out.println("Name: " + name);
+        System.out.println("ID Number: " + idNumber);
+        System.out.println("------------------------");
+        System.out.println("Result: The sum of " + num1 + " and " + num2 + " is " + sum);
+    }
+}
